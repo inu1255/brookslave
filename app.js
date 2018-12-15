81ed138d8112abbd422ea93459a98effab9555fe
inu1255 <929909260@qq.com>
2018-12-15 14:46:44 +0800

    first

--- app.js
+++ app.js
@@ -1,14 +1,62 @@
 const child_process = require('child_process');
 const axios = require('axios').default;
 
-async function main(token) {
-    let url = 'https://ssr.inu1255.cn/api/node/brook?token=' + token;
-    let { data } = await axios.get(url);
-    let args = ['servers'];
-    for (let item of data.data) {
-        args.push('-l', `:${item.port} ${item.passwd}`);
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+class BrookSlave {
+    constructor() {
+        this.prevCmd = '';
+        this.prevExit;
+        this.prev;
     }
-    console.log('brook', args);
+    async init() {
+        var { data } = await axios.get('https://ssr.inu1255.cn/api/node/gen');
+        console.log(data.data.msg);
+        this.token = data.data.token;
+    }
+    async loop() {
+        let url = 'https://ssr.inu1255.cn/api/node/brook?token=' + this.token;
+        var { data } = await axios.get(url);
+        let args = ['servers'];
+        for (let item of data.data) {
+            args.push('-l', `:${item.port} ${item.passwd}`);
+        }
+        let cmd = args.join(' ');
+        if (this.prevCmd != cmd) {
+            console.log(new Date().toLocaleString(), 'brook', cmd);
+            this.prevCmd = cmd;
+            if (this.prevExit) {
+                await this.prevExit();
+            }
+            if (args.length > 1) {
+                let proc = child_process.spawn('brook', args);
+                let pms = new Promise((resolve, reject) => {
+                    proc.on('exit', resolve);
+                });
+                this.prevExit = function() {
+                    proc.kill('SIGINT');
+                    return pms;
+                };
+                proc.stdout.on('data', console.log);
+                proc.stderr.on('data', console.log);
+            }
+        }
+    }
+}
+
+async function main() {
+    let slave = new BrookSlave();
+    await slave.init();
+    while (true) {
+        try {
+            await slave.loop();
+        } catch (error) {
+            console.log(error);
+        }
+        await sleep(5e3);
+    }
 }
 
 main.apply(null, process.argv.slice(2)).then(function() {
